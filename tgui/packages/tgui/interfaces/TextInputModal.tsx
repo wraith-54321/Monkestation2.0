9164713231e117531ba1b4a8023cbb321fcca47d
Lucy <lucy@absolucy.moe>
2025-03-17 06:35:40 +0000

    Some misc upstream fixes (mostly 516 and movement related) (#5945)
    ## About The Pull Request
    
    ports the following PRs from tg:
    
    - https://github.com/tgstation/tgstation/pull/75029
    - https://github.com/tgstation/tgstation/pull/75103
    - https://github.com/tgstation/tgstation/pull/79322
    - https://github.com/tgstation/tgstation/pull/81464
    - https://github.com/tgstation/tgstation/pull/81505
    - https://github.com/tgstation/tgstation/pull/81869
    - https://github.com/tgstation/tgstation/pull/86692
    - https://github.com/tgstation/tgstation/pull/89956
    - https://github.com/tgstation/tgstation/pull/90039
    - ~~https://github.com/tgstation/tgstation/pull/90057~~
    
    ## Why It's Good For The Game
    
    ## Changelog
    
    :cl:
    code: (nikothedude) TGUI input datums can now accept custom ui_states.
    fix: (Ghommie) Fixed the tgui text input trimming the last character of
    the input if it hits the maximum length.
    fix: (itsmeow) Fixed TGUI assets and icons sometimes not showing up
    until you refreshed the page on 516 clients.
    fix: (Aylong) Fixed TGUI text inputs that allows only 1 symbol instead
    infinity.
    /:cl:
    
    ---------
    
    Co-authored-by: Ghom <42542238+Ghommie@users.noreply.github.com>
    Co-authored-by: nikothedude <59709059+nikothedude@users.noreply.github.com>
    Co-authored-by: san7890 <the@san7890.com>
    Co-authored-by: Kyle Spier-Swenson <kyleshome@gmail.com>
    Co-authored-by: Aylong <69762909+AyIong@users.noreply.github.com>
    Co-authored-by: LemonInTheDark <58055496+LemonInTheDark@users.noreply.github.com>

--- tgui/packages/tgui/interfaces/TextInputModal.tsx
+++ tgui/packages/tgui/interfaces/TextInputModal.tsx
@@ -78,7 +78,7 @@ export const TextInputModal = (props) => {
             <Stack.Item>
               <InputButtons
                 input={input}
-                message={`${input.length}/${max_length}`}
+                message={`${input.length}/${max_length || '∞'}`}
               />
             </Stack.Item>
           </Stack>
