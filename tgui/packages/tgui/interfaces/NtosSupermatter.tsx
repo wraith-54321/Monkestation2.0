414ace2dcb75e0540dc86ad57ffcfb9a70f1fbf6
John Willard <53777086+JohnFulpWillard@users.noreply.github.com>
2026-01-02 02:47:34 -0500

    NT CIMS is scrollable (#10234)
    ## About The Pull Request
    
    NT CIMS is scrollable if there's too many SM's to fit on-screen.
    
    ## Why It's Good For The Game
    
    Closes github.com/Monkestation/Monkestation2.0/issues/10199 for the guy
    who manages it.
    
    ## Testing
    
    <img width="695" height="376" alt="image"
    src="https://github.com/user-attachments/assets/fbe586c9-ceca-437e-8dbb-c544a6b69f26"
    />
    
    <img width="697" height="294" alt="image"
    src="https://github.com/user-attachments/assets/6c1c9c21-28d9-44f8-8f9c-4e5cd785b101"
    />
    
    ## Changelog
    
    :cl:
    fix: NT CIMS is scrollable if you have enough supermatters.
    /:cl:

--- tgui/packages/tgui/interfaces/NtosSupermatter.tsx
+++ tgui/packages/tgui/interfaces/NtosSupermatter.tsx
@@ -26,6 +26,8 @@ export const NtosSupermatter = (props) => {
         ) : (
           <Section
             title="Detected Supermatters"
+            height="100%"
+            overflowY="auto"
             buttons={
               <Button
                 icon="sync"
