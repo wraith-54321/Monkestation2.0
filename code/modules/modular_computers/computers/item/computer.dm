wraith-54321/Monkestation2.0

commit dc065d166b3eb8e255911e3121e3e7d77b6cb3d4

PDA Apps Freezing When Recieving A Message (#10149)
## About The Pull Request
redacted for AI usage. changelog will be kept. PR has been rewritten by
@JohnFulpWillard
 
## Testing

redacted for AI usage.

## Changelog

:cl:
fix: Fixed an issue where receiving a PDA message could freeze other
active TGUI interfaces
code: Deferred PDA messenger UI updates to prevent overlapping TGUI
refreshes
/:cl:

## Pre-Merge Checklist
- [x] You tested this on a local server.
- [x] This code did not runtime during testing.
- [x] You documented all of your changes.

---------

Co-authored-by: John Willard <53777086+JohnFulpWillard@users.noreply.github.com>

code/modules/modular_computers/computers/item/computer.dm
@@ -529,7 +529,7 @@
 	loc.visible_message(span_notice("<img class='icon' src='\ref[src]'> \The [src] displays a [origin.filedesc] notification: [html_encode(alerttext)]"), vision_distance = vision_distance, push_appearance = src)
 
 /obj/item/modular_computer/proc/ring(ringtone, list/balloon_alertees) // bring bring
-	if(!use_energy())
+	if(!use_energy(check_programs = FALSE))
 		return
 	// Get the messenger app's new sound settings || Monkestation Addition START
 	var/sound_to_play = 'sound/machines/twobeep_high.ogg' //defaults to the original

code/modules/modular_computers/file_system/programs/messenger/messenger_program.dm
@@ -710,8 +710,9 @@
 	if (alert_able && should_ring)
 		computer.ring(ringtone, list(receiver_mob))
 
-	SStgui.update_uis(computer)
-	update_pictures_for_all()
+	if(computer.active_program == src)
+		SStgui.update_uis(computer)
+		update_pictures_for_all()
 
 /// topic call that answers to people pressing "(Reply)" in chat
 /datum/computer_file/program/messenger/Topic(href, href_list)

commit 4ba94ce2a3b7c2840753bd65bdfa3245e40636f4

Fix PDAs not ringing (#10450)
<!-- Write **BELOW** The Headers and **ABOVE** The comments else it may
not be viewable. -->
<!-- You can view Contributing.MD for a detailed description of the pull
request process. -->

## About The Pull Request

- Partially reverts bug introduced in #10149 with `check_programs =
FALSE`

## Changelog

<!-- If your PR modifies aspects of the game that can be concretely
observed by players or admins you should add a changelog. If your change
does NOT meet this description, remove this section. Be sure to properly
mark your PRs to prevent unnecessary GBP loss. You can read up on GBP
and it's effects on PRs in the tgstation guides for contributors. Please
note that maintainers freely reserve the right to remove and add tags
should they deem it appropriate. You can attempt to finagle the system
all you want, but it's best to shoot for clear communication right off
the bat. -->

:cl:
fix: PDAs should ring now
/:cl:

<!-- Both :cl:'s are required for the changelog to work! You can put
your name to the right of the first :cl: if you want to overwrite your
GitHub username as author ingame. -->
<!-- You can use multiple of the same prefix (they're only used for the
icon ingame) and delete the unneeded ones. Despite some of the tags,
changelogs should generally represent how a player might be affected by
the changes rather than a summary of the PR's contents. -->

## Pre-Merge Checklist
<!-- Don't bother filling these in while creating your Pull Request,
just click the checkboxes after the Pull Request is opened and you are
redirected to the page. -->
- [x] You tested this on a local server.
- [x] This code did not runtime during testing.
- [] You documented all of your changes.
<!-- Neither the compiler nor workflow checks are perfect at detecting
runtimes and errors. It is important to test your code/feature/fix
locally. -->

code/modules/modular_computers/computers/item/computer.dm
@@ -529,7 +529,7 @@
 	loc.visible_message(span_notice("<img class='icon' src='\ref[src]'> \The [src] displays a [origin.filedesc] notification: [html_encode(alerttext)]"), vision_distance = vision_distance, push_appearance = src)
 
 /obj/item/modular_computer/proc/ring(ringtone, list/balloon_alertees) // bring bring
-	if(!use_energy(check_programs = FALSE))
+	if(!use_energy())
 		return
 	// Get the messenger app's new sound settings || Monkestation Addition START
 	var/sound_to_play = 'sound/machines/twobeep_high.ogg' //defaults to the original