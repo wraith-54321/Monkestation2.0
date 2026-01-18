wraith-54321/Monkestation2.0

commit 7d78d5faf92c4a7e922f345857e9b98fe6ed23c7

Bundled React fixes (#10249)
## About The Pull Request
merging three separate projects to refactor inputs to match tgui core 🥲 

Updates various UI elements and interfaces. See changelog and testing
section screenshots.

## Why It's Good For The Game
yeah. it just is. 
## Testing
<img width="896" height="354" alt="image"
src="https://github.com/user-attachments/assets/cd167671-6d6c-4033-9e34-a43e3b517437"
/>

<img width="1132" height="456" alt="image"
src="https://github.com/user-attachments/assets/5fee5000-71ca-49d1-b2df-713211625e06"
/>

<img width="295" height="179" alt="image"
src="https://github.com/user-attachments/assets/3eac378d-79a9-4f67-853c-5ef55927ef28"
/>

## Changelog
:cl:
fix: various TGUI interface
admin: 2 new Secret panel buttons "Department Order Cooldown" and "Mass
Heal Everyone"
add: Robotact has a new menu for showing other borgs connected to the
same AI as you, as well as the status of your parent AI.
add: Seed extractor now shows the grind/juice results and plant reagent
volume, for seeds
code: adds a new unit test to check for duplicate loadout item names.
/:cl:
## Pre-Merge Checklist
- [x] You tested this on a local server.
- [x] This code did not runtime during testing.
- [x] You documented all of your changes.

---------

Co-authored-by: John Willard <53777086+JohnFulpWillard@users.noreply.github.com>
Co-authored-by: Flleeppyy <flleeppyy@gmail.com>
Co-authored-by: Chen Marisa <18307183+flleeppyy@users.noreply.github.com>
Co-authored-by: Lucy <lucy@absolucy.moe>

tgui/packages/tgui/interfaces/SeedExtractor.tsx
@@ -1,20 +1,20 @@
-import { BooleanLike } from 'common/react';
-import { createSearch } from 'common/string';
-import { flow } from 'common/fp';
-import { sortBy } from 'common/collections';
-import { useBackend, useLocalState } from '../backend';
+import { sortBy } from 'es-toolkit';
+import { useState } from 'react';
 import {
-  Input,
-  Tooltip,
   Box,
-  ProgressBar,
   Button,
+  Icon,
+  Input,
+  NoticeBox,
+  ProgressBar,
   Section,
   Table,
-  NoticeBox,
-  Icon,
-  DmIcon,
-} from '../components';
+  Tooltip,
+} from 'tgui-core/components';
+import { classes } from 'tgui-core/react';
+import { createSearch } from 'tgui-core/string';
+
+import { useBackend } from '../backend';
 import { Window } from '../layouts';
 
 type TraitData = {
@@ -26,7 +26,7 @@ type TraitData = {
 
 type ReagentData = {
   name: string;
-  rate: string;
+  rate: number;
 };
 
 type SeedData = {
@@ -41,10 +41,14 @@ type SeedData = {
   potency: number;
   instability: number;
   icon: string;
-  icon_state: string;
-  volume_mod: BooleanLike;
+  volume_mod: number;
+  volume_units: number;
   traits: string[];
   reagents: ReagentData[];
+  mutatelist: string[];
+  grind_results: string[];
+  juice_results: string[];
+  distill_reagent: string;
 };
 
 type SeedExtractorData = {
@@ -57,18 +61,19 @@ type SeedExtractorData = {
 
 export const SeedExtractor = (props) => {
   const { act, data } = useBackend<SeedExtractorData>();
-  const [searchText, setSearchText] = useLocalState('searchText', '');
-  const [sortField, setSortField] = useLocalState('sortField', 'name');
-  const [action, toggleAction] = useLocalState('action', true);
-  const search = createSearch(searchText, createSearchableString);
+  const [searchText, setSearchText] = useState('');
+  const [sortField, setSortField] = useState('name');
+  const [action, toggleAction] = useState(true);
+  const search = createSearch(searchText, (item: SeedData) => item.name);
   const seeds_filtered =
     searchText.length > 0 ? data.seeds.filter(search) : data.seeds;
-  const seeds = flow([
-    sortBy((item: SeedData) => item[sortField as keyof SeedData]),
-  ])(seeds_filtered || []);
+  const seeds = sortBy(seeds_filtered || [], [
+    (item: SeedData) => item[sortField as keyof SeedData],
+  ]);
   sortField !== 'name' && seeds.reverse();
+
   return (
-    <Window width={1080} height={400}>
+    <Window width={800} height={500}>
       <Window.Content scrollable>
         <Section>
           <Table>
@@ -76,97 +81,103 @@ export const SeedExtractor = (props) => {
               <Table.Cell colSpan={3} px={1} py={2}>
                 <Input
                   autoFocus
-                  placeholder={'Search...'}
+                  placeholder="Search..."
                   value={searchText}
-                  onInput={(e, value) => setSearchText(value)}
+                  onChange={setSearchText}
                   fluid
                 />
               </Table.Cell>
               <Table.Cell collapsing p={1}>
                 <Tooltip
                   content={
-                    'Determines product mass, reagent volume and strength of effects.'
+                    'Potency: Determines product mass, reagent volume and strength of effects.'
                   }
                 >
                   <Box
                     style={{ cursor: 'pointer' }}
                     onClick={(e) => setSortField('potency')}
                   >
-                    Potency
+                    PTN
                   </Box>
                 </Tooltip>
               </Table.Cell>
               <Table.Cell collapsing p={1}>
                 <Tooltip
                   content={
-                    'The number of products gathered in a single harvest.'
+                    'Yield: The number of products gathered in a single harvest.'
                   }
                 >
                   <Box
                     style={{ cursor: 'pointer' }}
                     onClick={(e) => setSortField('yield')}
                   >
-                    Yield
+                    YLD
                   </Box>
                 </Tooltip>
               </Table.Cell>
               <Table.Cell collapsing p={1}>
                 <Tooltip
                   content={
-                    'The health pool of the plant that delays death. Affects fermentation quality.'
+                    'Instability: The likelihood of the plant to randomize stats or mutate. Affects quality of resulting food & drinks.'
+                  }
+                >
+                  <Box
+                    style={{ cursor: 'pointer' }}
+                    onClick={(e) => setSortField('instability')}
+                  >
+                    INS
+                  </Box>
+                </Tooltip>
+              </Table.Cell>
+              <Table.Cell collapsing p={1}>
+                <Tooltip
+                  content={
+                    'Endurance: The health pool of the plant that delays death. Improves quality of resulting food & drinks.'
                   }
                 >
                   <Box
                     style={{ cursor: 'pointer' }}
                     onClick={(e) => setSortField('endurance')}
                   >
-                    Endurance
+                    END
                   </Box>
                 </Tooltip>
               </Table.Cell>
               <Table.Cell collapsing p={1}>
                 <Tooltip
-                  content={`The age at which the plant starts decaying, in ${data.cycle_seconds} second long cycles. Affects fermentation quality.`}
+                  content={`Lifespan: The age at which the plant starts decaying, in ${data.cycle_seconds} second long cycles. Improves quality of resulting food & drinks.`}
                 >
                   <Box
                     style={{ cursor: 'pointer' }}
                     onClick={(e) => setSortField('lifespan')}
                   >
-                    Lifespan
+                    LFS
                   </Box>
                 </Tooltip>
               </Table.Cell>
               <Table.Cell collapsing p={1}>
                 <Tooltip
-                  content={`The age required for the first harvest, in ${data.cycle_seconds} second long cycles.`}
+                  content={`Maturation: The age required for the first harvest, in ${data.cycle_seconds} second long cycles.`}
                 >
                   <Box
                     style={{ cursor: 'pointer' }}
                     onClick={(e) => setSortField('maturation')}
                   >
-                    Maturation
+                    MTR
                   </Box>
                 </Tooltip>
               </Table.Cell>
               <Table.Cell collapsing p={1}>
                 <Tooltip
-                  content={`The period of product regrowth, in ${data.cycle_seconds} second long cycles.`}
+                  content={`Production: The period of product regrowth, in ${data.cycle_seconds} second long cycles.`}
                 >
                   <Box
                     style={{ cursor: 'pointer' }}
                     onClick={(e) => setSortField('production')}
                   >
-                    Production
+                    PRD
                   </Box>
                 </Tooltip>
-              </Table.Cell>
-              <Table.Cell collapsing p={1}>
-                <Box
-                  style={{ cursor: 'pointer' }}
-                  onClick={(e) => setSortField('amount')}
-                >
-                  Amount
-                </Box>
               </Table.Cell>
               <Table.Cell collapsing p={1} textAlign="right">
                 {sortField !== 'name' && (
@@ -197,17 +208,13 @@ export const SeedExtractor = (props) => {
                   style={{ borderTop: '2px solid #222' }}
                 >
                   <Table.Cell collapsing>
-                    <DmIcon
+                    <Box
                       mb={-2}
-                      icon={item.icon}
-                      icon_state={item.icon_state}
-                      fallback={<Icon mr={1} name="spinner" spin />}
-                      height={'32px'}
-                      width={'32px'}
+                      className={classes(['seeds32x32', item.icon])}
                     />
                   </Table.Cell>
                   <Table.Cell py={0.5} px={1}>
-                    {item.name}
+                    {`${item.amount}x ${item.name}`}
                   </Table.Cell>
                   <Table.Cell py={0.5} px={1} collapsing textAlign={'right'}>
                     {item.traits?.map((trait) => (
@@ -217,17 +224,40 @@ export const SeedExtractor = (props) => {
                         trait_db={data.trait_db}
                       />
                     ))}
+                    {!!item.mutatelist.length && (
+                      <Tooltip
+                        content={`Mutates into: ${item.mutatelist.join(', ')}`}
+                      >
+                        <Icon name="dna" m={0.5} />
+                      </Tooltip>
+                    )}
                     {item.reagents.length > 0 && (
                       <Tooltip
                         content={
                           <ReagentTooltip
                             reagents={item.reagents}
+                            grind_results={item.grind_results}
                             potency={item.potency}
                             volume_mod={item.volume_mod}
+                            volume_units={item.volume_units}
                           />
                         }
                       >
                         <Icon name="blender" m={0.5} />
+                      </Tooltip>
+                    )}
+                    {!!item.juice_results && (
+                      <Tooltip
+                        content={`Juicing result: ${item.juice_results.join(', ')}`}
+                      >
+                        <Icon name="glass-water" m={0.5} />
+                      </Tooltip>
+                    )}
+                    {!!item.distill_reagent && (
+                      <Tooltip
+                        content={`Ferments into: ${item.distill_reagent}`}
+                      >
+                        <Icon name="wine-bottle" m={0.5} />
                       </Tooltip>
                     )}
                   </Table.Cell>
@@ -238,19 +268,19 @@ export const SeedExtractor = (props) => {
                     <Level value={item.yield} max={10} />
                   </Table.Cell>
                   <Table.Cell py={0.5} px={1} collapsing>
+                    <Level value={item.instability} max={100} reverse />
+                  </Table.Cell>
+                  <Table.Cell py={0.5} px={1} collapsing>
                     <Level value={item.endurance} max={100} />
                   </Table.Cell>
                   <Table.Cell py={0.5} px={1} collapsing>
                     <Level value={item.lifespan} max={100} />
                   </Table.Cell>
                   <Table.Cell py={0.5} px={1} collapsing>
-                    <Box textAlign="right">{item.maturation}</Box>
+                    <Box textAlign="center">{item.maturation}</Box>
                   </Table.Cell>
                   <Table.Cell py={0.5} px={1} collapsing>
-                    <Box textAlign="right">{item.production}</Box>
-                  </Table.Cell>
-                  <Table.Cell py={0.5} px={1} collapsing>
-                    <Box textAlign="right">{item.amount}</Box>
+                    <Box textAlign="center">{item.production}</Box>
                   </Table.Cell>
                   <Table.Cell
                     py={0.5}
@@ -296,7 +326,7 @@ export const SeedExtractor = (props) => {
   );
 };
 
-const Level = (props) => {
+export const Level = (props) => {
   return (
     <ProgressBar
       value={props.value}
@@ -325,43 +355,111 @@ const Level = (props) => {
   );
 };
 
-const ReagentTooltip = (props) => {
+export const ReagentTooltip = (props) => {
+  let rate_total = 0;
+  props.reagents.forEach((reagent) => {
+    rate_total += reagent.rate;
+  });
+  const reagent_volumes: number[] = [];
+  const reagent_percentages: number[] = [];
+  props.reagents.forEach((reagent) => {
+    reagent_percentages.push(reagent.rate / Math.max(1, rate_total));
+    reagent_volumes.push(
+      Math.max(
+        Math.round(
+          props.volume_units *
+            (props.potency / 100) *
+            (reagent.rate / Math.max(1, rate_total)) *
+            props.volume_mod,
+        ),
+        1,
+      ),
+    );
+  });
   return (
     <Table>
       <Table.Row header>
-        <Table.Cell>Reagents</Table.Cell>
+        <Table.Cell colSpan={3}>Reagents on grind:</Table.Cell>
       </Table.Row>
-      {props.reagents?.map((reagent) => (
-        <Table.Row key="">
+      {props.reagents?.map((reagent, i) => (
+        <Table.Row key={i}>
           <Table.Cell>{reagent.name}</Table.Cell>
           <Table.Cell py={0.5} pl={2} textAlign={'right'}>
-            {Math.max(
-              Math.round(reagent.rate * props.potency * props.volume_mod),
-              1,
-            )}
-            u
+            {reagent_volumes[i]}u
+          </Table.Cell>
+          <Table.Cell py={0.5} pl={2} textAlign={'right'}>
+            {rate_total > 1 && '~'}
+            {Math.round(reagent_percentages[i] * 100)}%
           </Table.Cell>
         </Table.Row>
       ))}
+      <Table.Row>
+        <Table.Cell colSpan={3} style={{ borderTop: '1px dotted gray' }} />
+      </Table.Row>
+      <Table.Row header>
+        <Table.Cell pt={1.5}>Total</Table.Cell>
+        <Table.Cell py={0.5} pl={2} textAlign={'right'}>
+          {Math.round(reagent_volumes.reduce((a, b) => a + b))}u
+        </Table.Cell>
+        <Table.Cell py={0.5} pl={2} textAlign={'right'}>
+          {Math.round(rate_total * 100)}%
+        </Table.Cell>
+      </Table.Row>
+      <Table.Row header>
+        <Table.Cell>Capacity</Table.Cell>
+        <Table.Cell py={0.5} pl={2} textAlign={'right'}>
+          {props.volume_units}u
+        </Table.Cell>
+        <Table.Cell />
+      </Table.Row>
+      {!!props.grind_results.length && (
+        <>
+          <Table.Row>
+            <Table.Cell colSpan={3} style={{ borderTop: '1px dotted gray' }} />
+          </Table.Row>
+          <Table.Row header>
+            <Table.Cell colSpan={3} pt={1}>
+              Nutriments turn into:
+            </Table.Cell>
+          </Table.Row>
+          {props.grind_results?.map((reagent, i) => (
+            <Table.Row key={i}>
+              <Table.Cell colSpan={3}>{reagent}</Table.Cell>
+            </Table.Row>
+          ))}
+        </>
+      )}
     </Table>
   );
 };
 
-const TraitTooltip = (props) => {
+export const TraitTooltip = (props) => {
   const trait = props.trait_db.find((t) => {
     return t.path === props.path;
   });
+  if (!trait) {
+    return;
+  }
   return (
     <Tooltip
-      key=""
       content={
         <Table>
+          {!!props.grafting && (
+            <Table.Row>
+              <Table.Cell pb={1}>Graft gains the following trait:</Table.Cell>
+            </Table.Row>
+          )}
           <Table.Row header>
             <Table.Cell>
               <Icon name={trait.icon} mr={1} />
               {trait.name}
             </Table.Cell>
           </Table.Row>
+          {!!props.removable && (
+            <Table.Row>
+              <Table.Cell pb={1}>Removable trait.</Table.Cell>
+            </Table.Row>
+          )}
           {!!trait.description && (
             <Table.Row>
               <Table.Cell>{trait.description}</Table.Cell>
@@ -370,16 +468,7 @@ const TraitTooltip = (props) => {
         </Table>
       }
     >
-      <Icon name={trait.icon} m={0.5} />
+      <Icon name={props.grafting ? 'scissors' : trait.icon} m={0.5} />
     </Tooltip>
   );
-};
-
-const createSearchableString = (seedData: SeedData): string => {
-  const reagentNames = seedData.reagents
-    .map((reagent) => reagent.name)
-    .join(' ');
-  const searchableString = `${seedData.name} ${reagentNames}`;
-
-  return searchableString;
 };

commit d1a78998fada50a1366310a702d505eafdaec0a6

fix seed extractor searching + icons (#10506)
## About The Pull Request

Fixes https://github.com/Monkestation/Monkestation2.0/issues/10500

This fixes some seed extractor stuff broken by
https://github.com/Monkestation/Monkestation2.0/pull/10249, and also
slightly refactors the seed extractor tgui code to use react hooks.

## Why It's Good For The Game

bugfix good

## Testing

![2026-01-16
(1768575301)](https://github.com/user-attachments/assets/6725da34-6a3e-470c-939d-85aec3301b87)

## Changelog
:cl:
fix: Fixed the seed extractor not showing seed icons.
fix: Fixed the seed extractor's search not searching for reagents.
/:cl:
## Pre-Merge Checklist
- [X] You tested this on a local server.
- [X] This code did not runtime during testing.
- [X] You documented all of your changes.

tgui/packages/tgui/interfaces/SeedExtractor.tsx
@@ -1,8 +1,9 @@
 import { sortBy } from 'es-toolkit';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import {
   Box,
   Button,
+  DmIcon,
   Icon,
   Input,
   NoticeBox,
@@ -11,7 +12,6 @@ import {
   Table,
   Tooltip,
 } from 'tgui-core/components';
-import { classes } from 'tgui-core/react';
 import { createSearch } from 'tgui-core/string';
 
 import { useBackend } from '../backend';
@@ -41,6 +41,7 @@ type SeedData = {
   potency: number;
   instability: number;
   icon: string;
+  icon_state: string;
   volume_mod: number;
   volume_units: number;
   traits: string[];
@@ -64,13 +65,28 @@ export const SeedExtractor = (props) => {
   const [searchText, setSearchText] = useState('');
   const [sortField, setSortField] = useState('name');
   const [action, toggleAction] = useState(true);
-  const search = createSearch(searchText, (item: SeedData) => item.name);
-  const seeds_filtered =
-    searchText.length > 0 ? data.seeds.filter(search) : data.seeds;
-  const seeds = sortBy(seeds_filtered || [], [
-    (item: SeedData) => item[sortField as keyof SeedData],
-  ]);
-  sortField !== 'name' && seeds.reverse();
+  const search = createSearch(searchText, (item: SeedData) =>
+    createSearchableString(item),
+  );
+  const seeds_filtered = useMemo(
+    () => (searchText.length > 0 ? data.seeds.filter(search) : data.seeds),
+    [searchText, search, data.seeds],
+  );
+  const seeds_sorted = useMemo(
+    () =>
+      sortBy(seeds_filtered || [], [
+        (item: SeedData) => item[sortField as keyof SeedData],
+      ]),
+    [seeds_filtered],
+  );
+
+  const seeds = useMemo(() => {
+    if (sortField !== 'name') {
+      return seeds_sorted.toReversed();
+    } else {
+      return seeds_sorted;
+    }
+  }, [sortField, seeds_sorted]);
 
   return (
     <Window width={800} height={500}>
@@ -85,6 +101,7 @@ export const SeedExtractor = (props) => {
                   value={searchText}
                   onChange={setSearchText}
                   fluid
+                  expensive
                 />
               </Table.Cell>
               <Table.Cell collapsing p={1}>
@@ -208,9 +225,13 @@ export const SeedExtractor = (props) => {
                   style={{ borderTop: '2px solid #222' }}
                 >
                   <Table.Cell collapsing>
-                    <Box
+                    <DmIcon
                       mb={-2}
-                      className={classes(['seeds32x32', item.icon])}
+                      icon={item.icon}
+                      icon_state={item.icon_state}
+                      fallback={<Icon mr={1} name="spinner" spin />}
+                      height={'32px'}
+                      width={'32px'}
                     />
                   </Table.Cell>
                   <Table.Cell py={0.5} px={1}>
@@ -471,4 +492,11 @@ export const TraitTooltip = (props) => {
       <Icon name={props.grafting ? 'scissors' : trait.icon} m={0.5} />
     </Tooltip>
   );
+};
+
+const createSearchableString = (seedData: SeedData): string => {
+  const reagentNames = seedData.reagents
+    .map((reagent) => reagent.name)
+    .join(' ');
+  return `${seedData.name} ${reagentNames}`;
 };